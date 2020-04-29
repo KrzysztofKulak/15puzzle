1bf23aed4d57198b6d3b1d8c0e616b13cab6b4e7
Krzysztof Kułak <krzysztofpiotrkulak@gmail.com>
2020-04-29 13:55:37 +0200

    add setting of desired state for a Game and win checking

--- game.py
+++ game.py
@@ -4,12 +4,16 @@ class Game:
         if type(initial) is list and len(initial) == 16:
             self.state = initial
         else:
-            self.__set_default_state()
+            self.state = self.__get_default_state()
+        self.desired_state = self.__get_default_state()
 
-    def __set_default_state(self):
+    @staticmethod
+    def __get_default_state():
+        default_state = list()
         for i in range(1, 16):
-            self.state.append(i)
-        self.state.append(None)
+            default_state.append(i)
+        default_state.append(None)
+        return default_state
 
     def __getitem__(self, item):
         return self.state[item]
@@ -22,4 +26,13 @@ class Game:
             f"{state_list[8:12]}\n"
             f"{state_list[12:]}\n"
         )
+
+    def __repr__(self):
+        return f"Game({self.state})"
+
+    def __bool__(self):
+        return self.state == self.desired_state
+
+    def check_if_won(self):
+        return bool(self)
 
